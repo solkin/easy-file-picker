a371c8a530929e3a0a99164f5adca228b1d9e3d8
Igor Solkin <inbox@tomclaw.com>
2017-06-11 23:56:40 +0300

    Initial commit

---
+++ app/src/main/java/com/tomclaw/filepicker/MainActivity.java
@@ -0,0 +1,59 @@
+package com.tomclaw.filepicker;
+
+import android.Manifest;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.greysonparrelli.permiso.Permiso;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PICK_FILE_REQUEST = 0x100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.pick_file).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pickFile();
+            }
+        });
+        Permiso.getInstance().setActivity(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Permiso.getInstance().setActivity(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Permiso.getInstance().onRequestPermissionResult(requestCode, permissions, grantResults);
+    }
+
+    private void pickFile() {
+        Permiso.getInstance().requestPermissions(new Permiso.IOnPermissionResult() {
+            @Override
+            public void onPermissionResult(Permiso.ResultSet resultSet) {
+                if (resultSet.isPermissionGranted(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    Intent intent = new Intent(MainActivity.this, FilePickerActivity.class);
+                    startActivityForResult(intent, PICK_FILE_REQUEST);
+                }
+            }
+
+            @Override
+            public void onRationaleRequested(Permiso.IOnRationaleProvided callback, String... permissions) {
+                Permiso.getInstance().showRationaleInDialog("Title", "Message", null, callback);
+            }
+        }, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+    }
+}
